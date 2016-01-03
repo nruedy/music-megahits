785b87db2453c8f303cdd36fac784602f2a02692
nruedy <nruedy@gmail.com>
2016-01-02 22:04:26 -0800

    Refactor scraping scripts for EN and Lyrics to try more variations

--- code/EchoNest_1_call_API.py
+++ code/EchoNest_1_call_API.py
@@ -6,10 +6,153 @@ import os, os.path
 import requests
 from pyechonest import config
 from pyechonest import song
+import Billboard_3a_clean_track_data_16_01_01b as BBclean
+import re
+
+
 
 timeout_sec = 20  # set timeout parameter for echonest song.search() and requests.get()
 config.CALL_TIMEOUT = timeout_sec
 
+class EchonestGetter(object):
+    '''
+    INPUTS: artist and song search terms
+    OUTPUTS: None
+    DESC: writes a json file with track information
+    '''
+    def __init__(self, artist, song, filename, tmstmp, track_num):
+        self._artist = artist
+        self._song = song
+        self._filename = filename
+        self._tmstmp = tmstmp
+        self._track_num = track_num
+
+    def run_search(self, search_artist, search_song):
+        try:
+            search_results = song.search(artist=search_artist, title=search_song, buckets=['song_hotttnesss'])
+        except Exception as inst:
+            search_results = []
+
+            print "Exception (basic search):", inst, "ID#:", self._track_num
+            with open('../data/echonest_error_logs/echonest_errors.csv', 'a') as outfile:
+                outfile.write(self._artist + '`' + self._song + '`' + 'Exc 1: ' + str(inst) +
+                                '`' + self._tmstmp + '\n')
+            time.sleep(1.0/3.0)
+
+        return search_results
+
+    def getdata(self):
+        filepath = '../data/echonest/{0}.json'.format(self._filename)
+        null_filepath = '../data/echonest_null/{0}.json'.format(self._filename)
+        error_filepath = '../data/echonest_null_results/{0}.csv'.format(self._tmstmp)
+
+        # Skip files retreived from a previous run
+        if os.path.exists(filepath):
+            return
+
+        # Skip files which could not be found on a previous run
+        if os.path.exists(null_filepath):
+            return
+
+        # Find the track, trying various search combinations
+
+        self._song = BBclean.fix_song_typos(self._song)
+
+        # create different versions of artist and song for searching
+
+        search_songs = [self._song]
+        search_artists = [self._artist]
+
+        feat_index = self._artist.lower().find(' feat')
+        if feat_index != -1:
+            search_artists.append(self._artist[:feat_index])
+
+        if clean_strings(self._artist) not in search_artists:
+            search_artists.append(clean_strings(self._artist))
+
+        if remove_stopwords(self._artist) not in search_artists:
+            search_artists.append(remove_stopwords(self._artist))
+
+        paren_index = self._song.find(' (')
+        if paren_index != -1:
+            search_songs.append(self._song[:paren_index])
+
+        if clean_strings(self._song) not in search_songs:
+            search_songs.append(clean_strings(self._song))
+
+        if remove_stopwords(self._song) not in search_songs:
+            search_songs.append(remove_stopwords(self._song))
+
+        # Run search with each combination of artist and song
+        search_results = []
+        for search_artist in search_artists:
+            for search_song in search_songs:
+                if search_results == []:
+                    search_results = self.run_search(search_artist, search_song)
+
+
+        # Write missing track data
+        if search_results == []:
+            with open(error_filepath, 'a') as outfile:
+                outfile.write(self._artist + '|' + self._song + '|' + str(self._track_num) + '\n')
+            with open(null_filepath, 'w') as outfile:
+                json.dump(search_results, outfile)
+            time.sleep(1.0/3.0)
+            return
+
+        try:
+            # get all the song objects that share the title with the most relevant song object
+            top_results = [track for track in search_results if track.title == search_results[0].title]
+            # of the songs with the same title, take the one with greatest "hotttnesss"
+            top_result = sorted(top_results, key=lambda x: x.song_hotttnesss)[-1]
+            results = dict()
+            results['id'] = top_result.id
+            results['title'] = top_result.title
+            results['artist_id'] = top_result.artist_id
+            results['artist_name'] = top_result.artist_name
+            results['audio_summary'] = top_result.audio_summary
+            results['song_type'] = top_result.song_type
+            results['artist_familiarity'] = top_result.artist_familiarity
+            results['artist_hotttnesss'] = top_result.artist_hotttnesss
+            results['artist_location'] = top_result.artist_location
+            results['song_currency'] = top_result.song_currency
+            results['song_discovery'] = top_result.song_discovery
+            results['song_hotttnesss'] = top_result.song_hotttnesss
+
+            #get detailed data
+            url = results['audio_summary']['analysis_url']
+            data = requests.get(url, timeout=timeout_sec).content
+            results['analysis'] = json.loads(data)
+
+            with open(filepath, 'w') as outfile:
+                json.dump(results, outfile)
+
+        except Exception as inst:
+            print "Exception (write): ", inst, "ID#:", self._track_num
+            with open('../data/echonest_error_logs/echonest_errors.csv', 'a') as outfile:
+                outfile.write(self._artist + '`' + self._song + '`' + 'Exc 2: ' + str(inst) +
+                                '`' + self._tmstmp + '\n')
+            time.sleep(1.0/3.0)
+            return
+
+
+def clean_strings(txt):
+    # replace & with 'and'
+    txt = re.sub('&', 'and', txt)
+    # replace ' with ' '
+    txt = re.sub("'", ' ', txt)
+    # remove punctuation
+    invalid_punc = '[!"#$%&\'()*+,-./:;<=>?@[\\]^`{|}~§]'
+    txt = re.sub(invalid_punc, '', txt)
+    # remove extra white space and make lowercase
+    return ' '.join(txt.lower().split())
+
+def remove_stopwords(txt):    # remove 'a', 'the', and extraneous white space
+    # check first if txt has more than one word, because there is an artist called "A"
+    if len(txt.split()) == 1:
+        return ' '.join(txt.split())
+    else:
+        return ' '.join([word for word in txt.split() if word.lower() not in ['the', 'a']])
 
 
 def get_API_data(start=0, end=None, input_filename='../data/billboard_tracks.pkl'):
@@ -34,7 +177,9 @@ def get_API_data(start=0, end=None, input_filename='../data/billboard_tracks.pkl
     downloaded_start = downloaded
 
     for track_tuple in track_info:
-        call_EN_API(track_tuple[0], track_tuple[1], track_tuple[2], tmstmp, count)
+        echonest_getter = EchonestGetter(track_tuple[0], track_tuple[1], track_tuple[2], tmstmp, count)
+        echonest_getter.getdata()
+
         if count % 20 == 0:
             downloaded_prev = downloaded
             downloaded = len(os.walk(EN_path).next()[2])
@@ -43,103 +188,6 @@ def get_API_data(start=0, end=None, input_filename='../data/billboard_tracks.pkl
                                                                      downloaded - downloaded_prev,
                                                                      downloaded - downloaded_start)
         count += 1
-
-
-def call_EN_API(artist, song_name, filename, tmstmp, count):
-    '''
-    INPUTS: artist and song search terms
-    OUTPUTS: None
-    DESC: writes a json file with track information
-    '''
-    #ipdb.set_trace()
-
-    filepath = '../data/echonest/{0}.json'.format(filename)
-    null_filepath = '../data/echonest_null/{0}.json'.format(filename)
-    error_filepath = '../data/echonest_null_results/{0}.csv'.format(tmstmp)
-
-    if os.path.exists(filepath):
-        return
-    if os.path.exists(null_filepath):
-        return
-
-    search_results = []
-
-    try:
-        search_results = song.search(artist=artist, title=song_name, buckets=['song_hotttnesss'])
-    except Exception as inst:
-        print "Exception (basic search):", inst, "ID#:", count
-        with open('../data/echonest_error_logs/echonest_errors.csv', 'a') as outfile:
-            outfile.write(artist + '`' + song_name + '`' + 'Exc 1: ' + str(inst) +
-                            '`' + tmstmp + '\n')
-        time.sleep(1.0/3.0)
-
-    if search_results == []:
-        feat_index = artist.lower().find(' feat')
-        if feat_index != -1:
-            try:
-                search_results = song.search(artist=artist[:feat_index], title=song_name, buckets=['song_hotttnesss'])
-            except Exception as inst:
-                print "Exception (feat): ", inst, "ID#:", count
-                with open('../data/echonest_error_logs/echonest_errors.csv', 'a') as outfile:
-                    outfile.write(artist + '`' + song_name + '`' + 'Exc 1 (feat): ' + str(inst) +
-                                    '`' + tmstmp + '\n')
-                time.sleep(1.0/3.0)
-
-    if search_results == []:
-        paren_index = song_name.find(' (')
-        if paren_index != -1:
-            try:
-                search_results = song.search(artist=artist, title=song_name[:paren_index], buckets=['song_hotttnesss'])
-            except Exception as inst:
-                print "Exception (paren): ", inst, "ID#:", count
-                with open('../data/echonest_error_logs/echonest_errors.csv', 'a') as outfile:
-                    outfile.write(artist + '`' + song_name + '`' + 'Exc 1 (paren): ' + str(inst) +
-                                    '`' + tmstmp + '\n')
-                time.sleep(1.0/3.0)
-
-
-    if search_results == []:
-        with open(error_filepath, 'a') as outfile:
-            outfile.write(artist + '|' + song_name + '\n')
-        with open(null_filepath, 'w') as outfile:
-            json.dump(search_results, outfile)
-        time.sleep(1.0/3.0)
-        return
-
-    try:
-        # get all the song objects that share the title with the most relevant song object
-        top_results = [track for track in search_results if track.title == search_results[0].title]
-        # of the songs with the same title, take the one with greatest "hotttnesss"
-        top_result = sorted(top_results, key=lambda x: x.song_hotttnesss)[-1]
-        results = dict()
-        results['id'] = top_result.id
-        results['title'] = top_result.title
-        results['artist_id'] = top_result.artist_id
-        results['artist_name'] = top_result.artist_name
-        results['audio_summary'] = top_result.audio_summary
-        results['song_type'] = top_result.song_type
-        results['artist_familiarity'] = top_result.artist_familiarity
-        results['artist_hotttnesss'] = top_result.artist_hotttnesss
-        results['artist_location'] = top_result.artist_location
-        results['song_currency'] = top_result.song_currency
-        results['song_discovery'] = top_result.song_discovery
-        results['song_hotttnesss'] = top_result.song_hotttnesss
-
-        #get detailed data
-        url = results['audio_summary']['analysis_url']
-        data = requests.get(url, timeout=timeout_sec).content
-        results['analysis'] = json.loads(data)
-
-        with open(filepath, 'w') as outfile:
-            json.dump(results, outfile)
-
-    except Exception as inst:
-        print "Exception (write): ", inst, "ID#:", count
-        with open('../data/echonest_error_logs/echonest_errors.csv', 'a') as outfile:
-            outfile.write(artist + '`' + song_name + '`' + 'Exc 2: ' + str(inst) +
-                            '`' + tmstmp + '\n')
-        time.sleep(1.0/3.0)
-        return
 
 
 
--- code/Lyrics_1_scrape.py
+++ code/Lyrics_1_scrape.py
@@ -1,11 +1,13 @@
+# -*- coding: utf-8 -*-
 import requests
 from bs4 import BeautifulSoup, Comment
 import re
 import pandas as pd
-import os.path
+import os, os.path
 import datetime
 import time
 import json
+import Billboard_3a_clean_track_data_16_01_01b as BBclean
 
 
 
@@ -17,64 +19,85 @@ def get_lyrics(start=0, end=None, input_filename='../data/billboard_tracks.pkl')
     '''
     df = pd.read_pickle(input_filename)
 
-    #extract these as lists?
-    artists = df.artist_clean.values
-    songs = df.song_clean.values
-    filenames = ['{0}___{1}'.format(artist, song) for artist, song in zip(artists, songs)]
+    artists = df.artist_orig.values
+    songs = df.song_orig.values
+    filenames = df.filename.values
     tracks = zip(artists, songs, filenames)
     tracks = tracks[ start : end ]
 
-   # set counter to keep track of progress (it adds one right away, so becomes 0 for the first entry)
+    # set counter to keep track of progress (it adds one right away, so becomes 0 for the first entry)
     count = -1
+    tot_num_files = len(tracks)
+    Lyrics_path = '../data/lyrics/'
+    downloaded = len(os.walk(Lyrics_path).next()[2])
+    downloaded_start = downloaded
 
 
     for track in tracks:
         artist, song, filename = track
         count += 1
         if count % 20 == 0:
-            print count, 'records processed'
+            downloaded_prev = downloaded
+            downloaded = len(os.walk(Lyrics_path).next()[2])
+            print '{0} / {1}, {2} just added, {3} new so far'.format(count,
+                                                         tot_num_files,
+                                                         downloaded - downloaded_prev,
+                                                         downloaded - downloaded_start)
 
         # check if lyrics are in folder:
         filepath = '../data/lyrics/{0}.json'.format(filename)
         if os.path.exists(filepath):
             continue
 
-        # check if lyrics are in null folder:
-        null_filepath = '../data/lyrics_null/{0}.json'.format(filename)
-        if os.path.exists(null_filepath):
-            continue
+        # generate artist and song strings for searching
+        # Find the track, trying various search combinations
 
-        # initialize results now, so that I can check length after searching lyrics.com
+        song = BBclean.fix_song_typos(song)
+
+        # create different versions of artist and song for searching
+
+        search_songs = [song]
+        search_artists = [artist]
+
+        feat_index = artist.lower().find(' feat')
+        if feat_index != -1:
+            search_artists.append(artist[:feat_index])
+
+        if clean_strings(artist) not in search_artists:
+            search_artists.append(clean_strings(artist))
+
+        if remove_stopwords(artist) not in search_artists:
+            search_artists.append(remove_stopwords(artist))
+
+        paren_index = song.find(' (')
+        if paren_index != -1:
+            search_songs.append(song[:paren_index])
+
+        if clean_strings(song) not in search_songs:
+            search_songs.append(clean_strings(song))
+
+        if remove_stopwords(song) not in search_songs:
+            search_songs.append(remove_stopwords(song))
+
+        # initialize results as empty dict
         results = dict()
 
-        # I commented out the lines below because I've already searched lyrics.com, so skipping to save time
-        '''
-        try:
-            soup = search_lyrics_dot_com(artist, song)
-            if soup != None:
-                results = extract_lyrics(soup, author_info=True)
-        except Exception as inst:
-            # print "Exception: Lyrics.com error", inst, artist, song, str(count)
-            pass
-        '''
-        if len(results) != 3:
-            try:
-                soup = search_songlyrics_dot_com(artist, song)
-                if soup != None:
-                    results = extract_lyrics(soup, author_info=False)
-                else:
-                    continue
-            except Exception as inst:
-                print "Exception: SongLyrics.com error", inst, artist, song, str(count)
-                # get current time stamp for error log
-                tmstmp = datetime.datetime.fromtimestamp(time.time()).strftime("%Y-%m-%d___%H-%M-%S")
-                with open('../data/lyrics_error_logs/songlyrics.csv', 'a') as outfile:
-                    outfile.write(artist + '`' + song + '`' + 'Exc 1: ' + str(inst) +
-                                    '`' + tmstmp + '`' + str(count) + '\n')
+        # Run search with each combination of artist and song, first in songlyrics.com, then lyrics.com
 
+        for search_artist in search_artists:
+            for search_song in search_songs:
+                if len(results) != 5:
+                    results = run_search_songlyrics(search_artist, search_song, count)
+
+        for search_artist in search_artists:
+            for search_song in search_songs:
+                if len(results) != 5:
+                    results = run_search_lyrics(search_artist, search_song, count)
+
+        # Write the file as a .json
 
         try:
-            if len(results) == 3:
+            if len(results) == 5:
                 with open(filepath, 'w') as outfile:
                     json.dump(results, outfile)
         except Exception as inst:
@@ -82,7 +105,7 @@ def get_lyrics(start=0, end=None, input_filename='../data/billboard_tracks.pkl')
             # get current time stamp for error log
             tmstmp = datetime.datetime.fromtimestamp(time.time()).strftime("%Y-%m-%d___%H-%M-%S")
             with open('../data/lyrics_error_logs/songlyrics.csv', 'a') as outfile:
-                outfile.write(artist + '`' + song + '`' + 'Exc 2: ' + str(inst) +
+                outfile.write(artist + '`' + song + '`' + 'Exc - Error Writing: ' + str(inst) +
                                 '`' + tmstmp + '`' + str(count) + '\n')
 
 
@@ -92,11 +115,8 @@ def transform_for_url(txt):
     OUTPUTS: string
     DESC: Transforms artist and song text to replace spaces with dashes, to create urls
     '''
-    # remove extra white space and make lowercase
-    txt = ' '.join(txt.lower().split())
-
     # replace spaces with dashes
-    return re.sub(' ', '-', txt)
+    return re.sub(' ', '-', clean_strings(txt))
 
 
 def transform_for_search(txt):
@@ -105,11 +125,111 @@ def transform_for_search(txt):
     OUTPUTS: string
     DESC: Transforms artist and song text to replace spaces with plusses, to create urls to run searches
     '''
-    # remove extra white space and make lowercase
-    txt = ' '.join(txt.lower().split())
-
     # replace spaces with plusses
-    return re.sub(' ', '+', txt)
+    return re.sub(' ', '+', clean_strings(txt))
+
+
+def clean_strings(txt):
+    '''
+    INPUTS: string
+    OUTPUTS: string
+    DESC: removes punctuation, makes text lowercase, and removes extra white space
+    '''
+    # replace & with 'and'
+    txt = re.sub('&', 'and', txt)
+    # replace ' with ' ' Note: It may be beneficial to run with and without (I already ran without)
+    txt = re.sub("'", ' ', txt)
+    # remove punctuation
+    invalid_punc = '[!"#$%&\'()*+,-./:;<=>?@[\\]^`{|}~§]'
+    txt = re.sub(invalid_punc, '', txt)
+    # remove extra white space and make lowercase
+    return ' '.join(txt.lower().split())
+
+
+def remove_stopwords(txt):    # remove 'a', 'the', and extraneous white space
+    # check first if txt has more than one word, because there is an artist called "A"
+    if len(txt.split()) == 1:
+        return ' '.join(txt.split())
+    else:
+        return ' '.join([word for word in txt.split() if word.lower() not in ['the', 'a']])
+
+
+def run_search_songlyrics(artist, song, count):
+    try:
+        soup = search_songlyrics_dot_com(artist, song)
+        if soup is not None:
+            results = extract_lyrics(soup, source='SongLyrics.com')
+        else:
+            results = dict()
+    except Exception as inst:
+        results = dict()
+
+        print "Exception: SongLyrics.com error", inst, artist, song, str(count)
+        # get current time stamp for error log
+        tmstmp = datetime.datetime.fromtimestamp(time.time()).strftime("%Y-%m-%d___%H-%M-%S")
+        with open('../data/lyrics_error_logs/songlyrics.csv', 'a') as outfile:
+            outfile.write(artist + '`' + song + '`' + 'Exc - SongLyrics.com: ' + str(inst) +
+                            '`' + tmstmp + '`' + str(count) + '\n')
+    return results
+
+
+def run_search_lyrics(artist, song, count):
+    try:
+        soup = search_lyrics_dot_com(artist, song)
+        if soup is not None:
+            results = extract_lyrics(soup, source='Lyrics.com')
+        else:
+            results = dict()
+    except Exception as inst:
+        print "Exception: Lyrics.com error", inst, artist, song, str(count)
+        # get current time stamp for error log
+        tmstmp = datetime.datetime.fromtimestamp(time.time()).strftime("%Y-%m-%d___%H-%M-%S")
+        with open('../data/lyrics_error_logs/songlyrics.csv', 'a') as outfile:
+            outfile.write(artist + '`' + song + '`' + 'Exc - Lyrics.com: ' + str(inst) +
+                            '`' + tmstmp + '`' + str(count) + '\n')
+    return results
+
+
+def search_songlyrics_dot_com(artist, song):
+    '''
+    INPUTS: two strings (artist name, song name)
+    OUTPUTS: lyrics as a string, or None if unsuccessful
+    DESC: try constructing the url for lyrics.com, and if that doesn't work, run a search on the site
+    '''
+    url = 'http://www.songlyrics.com/{0}/{1}-lyrics/'.format(transform_for_url(artist), transform_for_url(song))
+    html_string = requests.get(url).content
+    soup = BeautifulSoup(html_string, 'html.parser')
+    # uncomment the lines below to enable search
+    # right now, search is commented out because the site's search is very flexible, and it will almost
+    # always return a result. I need to run an additional check to make sure the result is the correct one.
+    '''
+    # need to check what conditional statement below would work. unlike lyrics.com, I don't think it returns ''.
+    if html_string == '':
+        # make search request on songlyrics.com
+        keyword_for_search = '{0}+{1}'.format(transform_for_search(artist), transform_for_search(song))
+        url = 'http://www.songlyrics.com/index.php?section=search&searchW={0}&submit=Search'.format(keyword_for_search)
+        search_results = requests.get(url, allow_redirects=False)
+
+        #check if search_results are empty:
+        if search_results == '':
+            return None
+
+        soup_search_results = BeautifulSoup(search_results.content, 'html.parser')
+
+        # take first entry
+        search_results_link = soup_search_results.find('div',{'class': 'serpresult'}).find_all('a')[0].get('href')
+        url = search_results_link
+        html_string = requests.get(url).content
+        soup = BeautifulSoup(html_string, 'html.parser')
+    '''
+    if soup.find('p',{'id': 'songLyricsDiv'}).text[:18] == 'Sorry, we have no ':
+        return None
+
+    # if it found the website, but there are no lyrics
+    if soup.find('p',{'id': 'songLyricsDiv'}) is None:
+        return None
+
+    return soup.find('p',{'id': 'songLyricsDiv'}).text
 
 
 def search_lyrics_dot_com(artist, song):
@@ -120,6 +240,8 @@ def search_lyrics_dot_com(artist, song):
     '''
     url = 'http://www.lyrics.com/{0}-lyrics-{1}.html'.format(transform_for_url(song), transform_for_url(artist))
     html_string = requests.get(url, allow_redirects=False).content
+    # commenting out search, because it may not be reliable
+    '''
     if html_string == '':
         # make search request on lyrics.com
         keyword_for_search = '{0}+{1}'.format(transform_for_search(song), transform_for_search(artist))
@@ -136,88 +258,50 @@ def search_lyrics_dot_com(artist, song):
         search_results_link = soup_search_results.find('div',{'id': 'rightcontent'}).find_all('a')[0].get('href')
         url = 'http://www.lyrics.com' + search_results_link
         html_string = requests.get(url).content
-
+    '''
     # if it found the website through constructing the url, but there are no lyrics
     soup = BeautifulSoup(html_string, 'html.parser')
-    if soup.find('div',{'id': 'lyrics'}) == None:
-        with open(null_filepath, 'w') as outfile:
-            json.dump('', outfile)
+    if soup.find('div',{'id': 'lyrics'}) is None:
         return None
 
     return soup.find('div',{'id': 'lyrics'}).text
 
 
-def search_songlyrics_dot_com(artist, song):
-    '''
-    INPUTS: two strings (artist name, song name)
-    OUTPUTS: lyrics as a string, or None if unsuccessful
-    DESC: try constructing the url for lyrics.com, and if that doesn't work, run a search on the site
-    '''
-    url = 'http://www.songlyrics.com/{0}/{1}-lyrics/'.format(transform_for_url(artist), transform_for_url(song))
-    html_string = requests.get(url, allow_redirects=False).content
-    soup = BeautifulSoup(html_string, 'html.parser')
-    if soup.find('p',{'id': 'songLyricsDiv'}).text[:18] == 'Sorry, we have no ':
-        return None
-        # remove the return None, and uncomment the lines below to enable search
-        # right now, search is commented out because the site's search is very flexible, and it will almost
-        # always return a result. I need to run an additional check to make sure the result is the correct one.
-        '''
-        # make search request on lyrics.com
-        keyword_for_search = '{0}+{1}'.format(transform_for_search(artist), transform_for_search(song))
-        url = 'http://www.songlyrics.com/index.php?section=search&searchW={0}&submit=Search'.format(keyword_for_search)
-        search_results = requests.get(url, allow_redirects=False)
-
-        #check if search_results are empty:
-        if search_results == '':
-            return None
-
-        soup_search_results = BeautifulSoup(search_results.content, 'html.parser')
-
-        # take first entry
-        search_results_link = soup_search_results.find('div',{'class': 'serpresult'}).find_all('a')[0].get('href')
-        url = search_results_link
-        html_string = requests.get(url).content
-        soup = BeautifulSoup(html_string, 'html.parser')
-        '''
-
-    # if it found the website, but there are no lyrics
-    if soup.find('p',{'id': 'songLyricsDiv'}) == None:
-        with open(null_filepath, 'w') as outfile:
-            json.dump('', outfile)
-        return None
-
-    return soup.find('p',{'id': 'songLyricsDiv'}).text
-
-
-def extract_lyrics(lyrics, author_info=False):
+def extract_lyrics(lyrics, source):
     '''
     INPUTS: soup object, bool
     OUTPUTS: dictionary
     DESC: clean lyrics and count stanzas / lines, return as dictionary
-        author_info bool controls whether the code for removing author info runs
-        (The code is needed for lyrics.com, but not songlyrics.com.)
     '''
-    #TODO: [Chorus] [intro] [outro] [4x]
-    #TODO: \u2019 \u201c \u201d
     results = dict()
+    null_result = dict()
+    results['source'] = source
     lyrics = lyrics.replace('\r','')
     results['stanzas'] = lyrics.count('\n\n') + 1
     lyrics = lyrics.replace('\n\n','\n')
     results['lines'] = lyrics.count('\n') + 1
     lyrics = lyrics.replace('\n',' ')
     # check that lyrics exist
-    if lyrics[:25] == 'We do not have the lyrics':
-        return None
+    if (len(lyrics) < 100) & (lyrics.lower().find('do not have the lyrics') != -1):
+        return null_result
+    if (len(lyrics) < 100) & (lyrics.lower().find('sorry, we have no') != -1):
+        return null_result
+    if (len(lyrics) < 30) & (lyrics.lower().find('instrumental') != -1):
+        results['instrumental'] = True
+    else:
+        results['instrumental'] = False
     # remove contributor information
-    index_contrib = None
-    if author_info == True:
+    if source == 'Lyrics.com':
         index_contrib = lyrics.rfind('---')
         if index_contrib == -1:
             index_contrib = None
-    results['lyrics'] = lyrics[:index_contrib]
+        lyrics = lyrics[:index_contrib]
+    results['lyrics'] = lyrics
 
     return results
-    
+
+
+
 
 
 if __name__ == '__main__':
